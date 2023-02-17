0942fb2cd2c36369f11a85deb9aa3659761f7387
karasd3 <donaldkarasz@gmail.com>
2023-02-17 16:34:41 -0500

    wip

--- 1.4/Source/VAR/VAR.cs
+++ 1.4/Source/VAR/VAR.cs
@@ -16,9 +16,22 @@ using System.Xml;
 using MVCF.Reloading.Comps;
 using MVCF.VerbComps;
 using MVCF.Comps;
+using MVCF.Commands;
+using Verse.AI;
+using MVCF.Utilities;
 
 namespace VAR
 {
+    public class VanillaAmmoMod : Mod
+    {
+        public VanillaAmmoMod(ModContentPack pack) : base(pack)
+        {
+            Harmony harmony = new Harmony(id: "VAR.Mod");
+
+            harmony.Patch(AccessTools.Method(typeof(AdditionalVerbProps), "ConfigErrors", new Type[] { typeof(ThingDef) }),
+                          prefix: new HarmonyMethod(typeof(AdditionalVerbProps_ConfigErrors_Patch), "Prefix"));
+        }
+    }
     [StaticConstructorOnStartup]
     public static class VAR
     {
@@ -32,7 +45,7 @@ namespace VAR
         {
             public static void Postfix(ref Projectile __instance, ref int __result)
             {
-                float damage = __result * (__instance.GetComp<CompCustomProjectile>()?.Props.damageMultiplier ?? 1f) ;
+                float damage = __result * (__instance.GetComp<CompCustomProjectile>()?.damageMultiplier ?? 1f) ;
                 __result = (int)damage;
                 return;
             }
@@ -43,7 +56,7 @@ namespace VAR
         {
             public static void Postfix(ref Projectile __instance, ref float __result)
             {
-                __result *= __instance.GetComp<CompCustomProjectile>()?.Props.apMultiplier ?? 1f;
+                __result *= __instance.GetComp<CompCustomProjectile>()?.apMultiplier ?? 1f;
                 return;
             }
         }
@@ -65,24 +78,24 @@ namespace VAR
                 }
             }
         }
-        public static CompProperties_CustomProjectile customProjectile;
-        [HarmonyPatch(typeof(Verb_LaunchProjectile), "Projectile", MethodType.Getter)]
-        public static class Verb_LaunchProjectile_Projectile_Patch 
-        {
-            public static void Postfix(ref Verb __instance, ref ThingDef __result)
-            {
-                if (__result == null)
-                {
-                    return;
-                }
-                CompProperties_CustomProjectile customprojectiles = __result.GetCompProperties<CompProperties_CustomProjectile>();
-                if (customprojectiles != null)
-                {
-                    __result = __instance.verbProps.defaultProjectile;
-                    customProjectile = customprojectiles;
-                }
-            }
-        }
+        //public static CompProperties_CustomProjectile customProjectile;
+        //[HarmonyPatch(typeof(Verb_LaunchProjectile), "Projectile", MethodType.Getter)]
+        //public static class Verb_LaunchProjectile_Projectile_Patch
+        //{
+        //    public static void Postfix(Verb_LaunchProjectile __instance, ref ThingDef __result)
+        //    {
+        //        if (__result == null)
+        //        {
+        //            return;
+        //        }
+        //        CompProperties_CustomProjectile customprojectiles = __result.GetCompProperties<CompProperties_CustomProjectile>();
+        //        if (customprojectiles != null)
+        //        {
+        //            __result = __instance.verbProps.defaultProjectile;
+        //            customProjectile = customprojectiles;
+        //        }
+        //    }
+        //}
         [HarmonyPatch(typeof(Verb_LaunchProjectile), "TryCastShot")]
         public static class Verb_LaunchProjectile_TryCastShot_Patch//loads comp into projectile
         {
@@ -94,6 +107,7 @@ namespace VAR
                 {
                     if (instruction.opcode == OpCodes.Stloc_S && instruction.operand is LocalBuilder lb && lb.LocalIndex == 7)
                     {
+                        yield return new CodeInstruction(OpCodes.Ldarg_0);
                         yield return new CodeInstruction(OpCodes.Call, to);
                         yield return instruction;
                         continue;
@@ -184,38 +198,41 @@ namespace VAR
         {
             public static float Damage(float __result, Projectile __instance)
             {
-                return __result * (__instance.GetComp<CompCustomProjectile>()?.Props.damageMultiplier ?? 1f);
+                return __result * (__instance.GetComp<CompCustomProjectile>()?.damageMultiplier ?? 1f);
             }
             public static float ArmorP(float __result, Projectile __instance)
             {
-                return __result * (__instance.GetComp<CompCustomProjectile>()?.Props.apMultiplier ?? 1f);
+                return __result * (__instance.GetComp<CompCustomProjectile>()?.apMultiplier ?? 1f);
             }
-            public static Projectile assignComp(Projectile projectile)
+            public static Projectile assignComp(Projectile projectile, Verb_LaunchProjectile verb)
             {
-                if (customProjectile != null)
+                VerbComp_Reloadable_ChangeableAmmo_Infinite comp = verb.Managed().TryGetComp<VerbComp_Reloadable_ChangeableAmmo_Infinite>();
+                if (comp != null && comp.Projectile != null)
                 {
-                    CompCustomProjectile compCustomProjectile = new CompCustomProjectile();
-                    compCustomProjectile.Initialize(customProjectile);
-                    projectile.AllComps.Add(compCustomProjectile);
-                    customProjectile = null;
+                    projectile.GetComp<CompCustomProjectile>().assignProps(comp.Projectile);
+                    comp.Projectile = null;
                 }
                 return projectile;
             }
             public static DamageDef assignDef(DamageDef damageDef, Projectile projectile)
             {
                 CompCustomProjectile comp = projectile.GetComp<CompCustomProjectile>();
-                if (comp != null && comp.Props.damageDef != null)
+                if (comp != null && comp.damageDef != null)
                 {
-                    return comp.Props.damageDef;
+                    return comp.damageDef;
                 }
                 return damageDef;
             }
-            public static void extraDamagesBullet(Projectile_Explosive projectile, Thing hitThing, BattleLogEntry_RangedImpact battleLogEntry_RangedImpact, bool instigatorGuilty)
+            public static void extraDamagesBullet(Bullet projectile, Thing hitThing, BattleLogEntry_RangedImpact battleLogEntry_RangedImpact, bool instigatorGuilty)
             {
+                if (hitThing == null)
+                {
+                    return;
+                }
                 CompCustomProjectile comp = projectile.GetComp<CompCustomProjectile>();
                 if (comp != null)
                 {
-                    foreach (ExtraDamage extraDamage in comp.Props.extraDamages)
+                    foreach (ExtraDamage extraDamage in comp.extraDamages)
                     {
                         if (Rand.Chance(extraDamage.chance))
                         {
@@ -230,7 +247,7 @@ namespace VAR
                 CompCustomProjectile comp = projectile.GetComp<CompCustomProjectile>();
                 if (comp != null)
                 {
-                    foreach (ExtraDamage extraDamage in comp.Props.extraDamages)
+                    foreach (ExtraDamage extraDamage in comp.extraDamages)
                     {
                         if (Rand.Chance(extraDamage.chance))
                         {
@@ -252,27 +269,188 @@ namespace VAR
         public float apMultiplier = 1f;
         //secondaries
         //adds extra damages, emp, burns, shocks, extinguish, flame, toxgas, etc
-        public List<ExtraDamage> extraDamages= new List<ExtraDamage>();
+        public List<ExtraDamage> extraDamages;
         //replaces damage def, ex. bullet toxic, hediff appliers which need/scale with damage values
         public DamageDef damageDef;
     }
+    //public class CustomProjectileHolder
+    //{
+    //    //primaries
+    //    public float damageMultiplier = 1f;
+    //    public float apMultiplier = 1f;
+    //    //secondaries
+    //    //adds extra damages, emp, burns, shocks, extinguish, flame, toxgas, etc
+    //    public List<ExtraDamage> extraDamages;
+    //    //replaces damage def, ex. bullet toxic, hediff appliers which need/scale with damage values
+    //    public DamageDef damageDef;
+
+    //    public CustomProjectileHolder(CompProperties_CustomProjectile Props)
+    //    {
+    //        damageMultiplier = Props.damageMultiplier;
+    //        apMultiplier = Props.apMultiplier;
+    //        extraDamages = Props.extraDamages;
+    //        damageDef = Props.damageDef;
+    //        Log.Message("test"+damageMultiplier.ToString()+Props.damageMultiplier.ToString());
+    //    }
+    //}
     public class CompCustomProjectile : ThingComp
     {
+        public float damageMultiplier = 1f;
+        public float apMultiplier = 1f;
+
+        public List<ExtraDamage> extraDamages = new List<ExtraDamage>();
+
+        public DamageDef damageDef;
         public CompProperties_CustomProjectile Props => (CompProperties_CustomProjectile)props;
+
+        public void assignProps(CompProperties_CustomProjectile customProjectile)
+        {
+            damageMultiplier=customProjectile.damageMultiplier;
+            Log.Message(damageMultiplier.ToString());
+            Log.Message(customProjectile.damageMultiplier.ToString());
+            apMultiplier=customProjectile.apMultiplier;
+            extraDamages=customProjectile.extraDamages;
+            damageDef=customProjectile.damageDef;
+        }
     }
     public class VerbComp_Reloadable_ChangeableAmmo_Infinite : VerbComp_Reloadable_ChangeableAmmo
     {
+        private readonly ThingOwner<Thing> loadedAmmo = new ThingOwner<Thing>();
+
+        public Thing nextAmmoItem;
+        public Pawn Pawn => parent?.Manager?.Pawn;
         public override bool Available()
         {
             return true;
         }
-
-        public override void Initialize(VerbCompProperties props)
+        public override IEnumerable<CommandPart> GetCommandParts(Command_VerbTargetExtended command)
         {
-            base.Initialize(props);
+            yield return new CommandPart_Reloadable_Infinite
+            {
+                parent = command,
+                Reloadable = this
+            };
+        }
+        public CompProperties_CustomProjectile Projectile;
+        public override ThingDef ProjectileOverride(ThingDef oldProjectile)
+        {
+            Log.Message("start projectile transfer");
+            //ThingDef def = nextAmmoItem?.def;
+
+            ThingDef def = base.ProjectileOverride(oldProjectile);
+            if (def != null)
+            {
+                Log.Message("found ammo, checking comp");
+                CompProperties_CustomProjectile customprojectiles = def.GetCompProperties<CompProperties_CustomProjectile>();
+                if (customprojectiles != null)
+                {
+                    Log.Message("found comp damage mult: " + customprojectiles.damageMultiplier.ToString());
+
+                    Projectile = customprojectiles;
+                }
+            }
+            else
+            {
+                Projectile = null;
+            }
+            return oldProjectile;
+        }
+        public override void Notify_ShotFired()
+        {
+            if (ShotsRemaining > 0)
+            {
+                ShotsRemaining--;
+            }
+            if (Pawn?.CurJobDef == JobDefOf.Hunt)
+            {
+                Pawn.jobs.EndCurrentJob(JobCondition.Incompletable);
+            }
+            if (nextAmmoItem != null)
+            {
+                nextAmmoItem.stackCount--;
+                if (nextAmmoItem.stackCount == 0)
+                {
+                    loadedAmmo.Remove(nextAmmoItem);
+                    nextAmmoItem.Destroy();
+                    nextAmmoItem = loadedAmmo.FirstOrFallback(null);
+                }
+            }
+        }
+    }
+
+    public class CommandPart_Reloadable_Infinite : CommandPart_Reloadable
+    {
+        public override void PostInit()
+        {
+        }
+    }
+
+    [HarmonyPatch(typeof(CompProperties_VerbProps), "PropsFor")]
+    public static class VerbProps_PropsFor_Patch
+    {
+        public static void Postfix(AdditionalVerbProps __result, Verb verb, List<AdditionalVerbProps> ___verbProps)
+        {
+            if (__result == null)
+            {
+                __result = ___verbProps?.FirstOrDefault(prop => prop.label == "CustomVARAmmo");
+            }
+        }
+    }
+    //[HarmonyPatch(typeof(CompProperties_VerbProps), "PostLoadSpecial")]
+    //public static class Reloadable_TargetVerb_Patch
+    //{
+    //    public static void Prefix(ref List<AdditionalVerbProps> ___verbProps, ThingDef parent)
+    //    {
+    //        foreach (AdditionalVerbProps props in ___verbProps)
+    //        {
+    //            if (props != null && props.label == "CustomVARAmmo")
+    //            {
+    //                props.label = parent.Verbs.FirstOrDefault((VerbProperties v) => v.label != null).label;
+    //                if (props.label==null ||props.label == "CustomVARAmmo")
+    //                {
+    //                    props.label = (parent.Verbs.FirstOrDefault((VerbProperties v) => v.defaultProjectile != null).defaultProjectile.ToString());
+
+    //                }
+    //            }
+    //        }
+    //    }
+    //}
+
+    public static class AdditionalVerbProps_ConfigErrors_Patch
+    {
+        public static void Prefix(ref string ___label, List<VerbCompProperties> ___comps, ThingDef parentDef)
+        {
+            //foreach (AdditionalVerbProps props in ___verbProps)
+            //{
+
+                if (___label == "CustomVARAmmo")
+                {
+                    ___label=parentDef.Verbs.FirstOrDefault((VerbProperties v) => v.label != null).label;
+                    
+                    if (___label == null || ___label == "CustomVARAmmo")
+                    {
+
+                        ___label = (parentDef.Verbs.FirstOrDefault((VerbProperties v) => v.defaultProjectile != null).defaultProjectile.ToString());
+
+                    }
+                    string label = ___label;
+
+                    VerbProperties props = parentDef.Verbs.FirstOrDefault((VerbProperties vb) => vb.label == label);
+                    if (props == null)
+                    {
+                        return;
+                    }
+                    foreach (VerbCompProperties comp in ___comps)
+                    {
+                        FixConfig((VerbCompProperties_Reloadable)comp, props, parentDef);
+                    }
+                }
+        }
+        public static void FixConfig(VerbCompProperties_Reloadable Props, VerbProperties VerbProps, ThingDef parent)
+        {
             if (Props.AmmoFilter == null)
             {
-                TechLevel tech = parent.Verb.EquipmentSource.def.techLevel;
+                TechLevel tech = parent.techLevel;
                 Props.AmmoFilter = new ThingFilter();
                 switch (tech)
                 {
@@ -320,62 +498,11 @@ namespace VAR
                         break;
                     default:
                         break;
-                }                    
+                }
             }
             if (Props.MaxShots == 0)
             {
-                Props.MaxShots = (int)(6 * (parent.Verb.verbProps.burstShotCount) / (parent.Verb.verbProps.defaultProjectile?.projectile.stoppingPower ?? 0.5));
-            }
-            ShotsRemaining = Props.StartLoaded ? Props.MaxShots : 0;
-        }
-    }
-    [HarmonyPatch(typeof(CompProperties_VerbProps), "PropsFor")]
-    public static class VerbProps_PropsFor_Patch
-    {
-        public static void Postfix(AdditionalVerbProps __result, Verb verb, List<AdditionalVerbProps> ___verbProps)
-        {
-            if (__result == null)
-            {
-                __result = ___verbProps?.FirstOrDefault(prop => prop.label == "CustomVARAmmo");
-            }
-        }
-    }
-    //[HarmonyPatch(typeof(CompProperties_VerbProps), "PostLoadSpecial")]
-    //public static class Reloadable_TargetVerb_Patch
-    //{
-    //    public static void Prefix(ref List<AdditionalVerbProps> ___verbProps, ThingDef parent)
-    //    {
-    //        foreach (AdditionalVerbProps props in ___verbProps)
-    //        {
-    //            if (props != null && props.label == "CustomVARAmmo")
-    //            {
-    //                props.label = parent.Verbs.FirstOrDefault((VerbProperties v) => v.label != null).label;
-    //                if (props.label==null ||props.label == "CustomVARAmmo")
-    //                {
-    //                    props.label = (parent.Verbs.FirstOrDefault((VerbProperties v) => v.defaultProjectile != null).defaultProjectile.ToString());
-
-    //                }
-    //            }
-    //        }
-    //    }
-    //}
-
-    [HarmonyPatch(typeof(VerbCompProperties), "PostLoadSpecial")]
-    public static class Reloadable_TargetVerb_Patch
-    {
-        public static void Prefix(ref List<AdditionalVerbProps> ___verbProps, ThingDef parent)
-        {
-            foreach (AdditionalVerbProps props in ___verbProps)
-            {
-                if (props != null && props.label == "CustomVARAmmo")
-                {
-                    props.label = parent.Verbs.FirstOrDefault((VerbProperties v) => v.label != null).label;
-                    if (props.label == null || props.label == "CustomVARAmmo")
-                    {
-                        props.label = (parent.Verbs.FirstOrDefault((VerbProperties v) => v.defaultProjectile != null).defaultProjectile.ToString());
-
-                    }
-                }
+                Props.MaxShots = (int)Mathf.Max((6f * (VerbProps.burstShotCount) / (VerbProps.defaultProjectile?.projectile.stoppingPower ?? 0.5f)),1f);
             }
         }
     }
